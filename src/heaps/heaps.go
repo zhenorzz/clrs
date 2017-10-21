59e825b2cb1c7a1be58474de2936c4351f292677
Zane <liz@uclbrt.com>
2017-10-21 17:34:12 +0800

    loop

--- src/heaps/heaps.go
+++ src/heaps/heaps.go
@@ -28,6 +28,31 @@ func MaxHeapify(A []int, i int) []int {
 	return A
 }
 
+func MaxHeapifyLoop(A []int, i int) []int {
+	length := len(A)
+	largest := i
+	for  {
+		left := Left(largest)
+		if length > left && A[left] > A[largest] {
+			largest = left
+		}
+		right := Right(largest)
+		if length > right && A[right] > A[largest] {
+			largest = right
+		}
+		if largest != i {
+			temp := A[i]
+			A[i] = A[largest]
+			A[largest] = temp
+			i = largest
+		} else {
+			break
+		}
+	}
+
+	return A
+}
+
 func MinHeapify(A []int, i int) []int {
 	length := len(A)
 	left := Left(i)
--- src/main/main.go
+++ src/main/main.go
@@ -6,5 +6,5 @@ import (
 )
 
 func main() {
-	fmt.Println(heaps.MaxHeapify([]int{27,17,14,6,13,10,1,5,7,12},3))
+	fmt.Println(heaps.MaxHeapifyLoop([]int{27,17,14,6,13,10,1,5,7,12},3))
 }
